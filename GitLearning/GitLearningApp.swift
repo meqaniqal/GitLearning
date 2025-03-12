bcf3b024c8015ee36f25979506ce351913a0b9d4
mekkanix <shruprunnaprake@gmail.com>
2025-03-12 13:28:24 -0700

    Initial Commit

---
+++ GitLearning/GitLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitLearningApp.swift
+//  GitLearning
+//
+//  Created by Sheldon Lawrence on 3/12/25.
+//
+
+import SwiftUI
+
+@main
+struct GitLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
