626764ae5b9815bd1fef9488fe93e84654e95623
mekkanix <shruprunnaprake@gmail.com>
2025-03-12 20:20:21 -0700

    refactor navigationLink to navigationDestination, changed some visual elements.

--- GitLearning/ContentView.swift
+++ GitLearning/ContentView.swift
@@ -9,32 +9,64 @@ import SwiftUI
 //test new donothing commit with only this comment as a change.
 struct ContentView: View {
     @State private var showSheet = false
+    @State private var showNavigation=false
     var body: some View {
         //enable NavigationLinks
         NavigationStack {
             VStack {
+                
                 //Here's Navigation link to DetailView()
-                NavigationLink {
-                    DetailView()
+//                NavigationLink {
+//                    DetailView()
                 //label for the link
-                } label: {
-                    Text("Git learning app")
+//                } label: {
+//                    Text("Git learning app")
+//                        .foregroundStyle(.black)
+//                        .background(Color.yellow)
+//                }
+                
+                //corresponds to the navigationDestination modifier below, activated by $showNavigation
+                Button(action: {
+                    showNavigation=true
+                }, label: {
+                    Text("Go to Detail View")
                         .foregroundStyle(.black)
+                        .padding(5)
                         .background(Color.yellow)
-                }
+                        .cornerRadius(10)
+                    })
+                .padding(.bottom, 20)
                 Button(action: {
                     showSheet=true
                 }, label:{
                     Text("Demonstrate Show other sheet")
+                       
+                        .foregroundStyle(.black)
+                        .padding(5)
                         .background(Color.yellow)
+                        .cornerRadius(10)
                 })
+                
               
             }
             .padding()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            
+            .background(Color.black.opacity(0.9))
+            
+            
+            .ignoresSafeArea()
             .sheet(isPresented: $showSheet, content: {
                 SheetView()
             })
+            .navigationDestination(isPresented: $showNavigation) {
+                DetailView()
+            }
+           
+            
         }
+        
+        
     }
 }
 
--- GitLearning/DetailView.swift
+++ GitLearning/DetailView.swift
@@ -10,6 +10,15 @@ import SwiftUI
 struct DetailView: View {
     var body: some View {
         Text("An app to learn git.")
+            .foregroundStyle(Color.red)
+            .font(.largeTitle)
+            .frame(width: 300, height: 300)
+            .background(Color.yellow)
+            .cornerRadius(50)
+            .border(Color.blue, width: 5)
+            .cornerRadius(50)
+            .frame(maxWidth: .infinity,maxHeight: .infinity)
+            .background(Color.black.opacity(0.9))
     }
 }
 
